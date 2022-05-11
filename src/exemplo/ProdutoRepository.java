94b2a764940f579888ea855d7cb9438691f3cf3b
Vinicius-Sousa1 <ferreiradesousavinicius@gmail.com>
2022-05-11 17:49:52 -0300

    repo de produto

--- src/exemplo/ProdutoRepository.java
+++ src/exemplo/ProdutoRepository.java
@@ -1,5 +1,35 @@
 package exemplo;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProdutoRepository {
 
+	private Map<String,Produto> catalogo;
+	
+	public ProdutoRepository() {
+		this.catalogo = new HashMap<>();
+	}
+	
+	public void adicionaProduto(Produto produto) {
+		this.catalogo.put(produto.getId(), produto);
+	}
+	
+	public void removeProduto(Produto produto) {
+		this.catalogo.remove(produto.getId());
+	}
+	
+	public void editaProduto() {
+		
+	}
+	
+	public Produto recuperaProduto(String id) {
+		return this.catalogo.get(id);
+	}
+	
+	public Collection<Produto> listaProdutos() {
+		return this.catalogo.values();
+	}
+	
 }
